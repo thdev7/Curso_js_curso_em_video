9d765b27c1bf3fb5f148c7642d787951c249c9b7
Thiago Andrade <thiagoadd14@gmail.com>
2024-09-11 11:16:22 -0300

    while e do

--- aula13/main.js
+++
@@ -1,5 +0,0 @@
-var c = 1
-while (c < 6) {
-    console.log('tudo bem')
-    c++ // igual a c = c + 1
-}
---
+++ aula13/while.js
@@ -0,0 +1,5 @@
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ // igual a c = c + 1
+}

c20d449376cde3d594372d2e8ad9a59d2c381e82
Thiago Andrade <thiagoadd14@gmail.com>
2024-10-01 14:51:14 -0300

    funções

---
+++ aula16/funcao.js
@@ -0,0 +1,15 @@
+//chamda
+function parimp(n) {
+    //ação
+    if (n%2 == 0) {
+        //retorno
+        return 'Par!'
+    }else {
+        return 'Impar!'
+    }
+}
+
+let res = parimp(5)
+console.log(res)
+
+//n é o parametro
